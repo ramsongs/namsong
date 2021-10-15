ca27a6fbf49186f54e418675e04e1aa30214faba
alpaca <zxcvbnm5288@gmail.com>
2021-10-15 19:54:35 +0900

    create faq page

--- src/routes/Faq.js
+++ src/routes/Faq.js
@@ -1,7 +1,20 @@
 export default function Faq() {
   return (
-    <div>
-      <span>haha</span>
-    </div>
+    <section className="faq">
+      <article className="faq-top">
+        <div className="faq-top__wrap">
+          <div>
+            <h2 className="faq-top__title">
+              도움이 필요하신가요? 저는 안잡니다
+            </h2>
+            <p className="faq-top__time">
+              평일 00:00 - 23:59<span className="separator">|</span>주말 00:00 -
+              23:59
+            </p>
+          </div>
+          <ul></ul>
+        </div>
+      </article>
+    </section>
   );
 }
