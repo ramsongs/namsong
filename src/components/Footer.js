53aeb85b80797dd6a7ed892d96e4271cf05005cb
alpaca <zxcvbnm5288@gmail.com>
2021-10-18 17:59:35 +0900

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ export default function Footer() {
           경기도 수원시 권선구 서호서로33번길 27, 1층 102호(서둔동)
         </address>
         <p>
-          사업자등록번호: 651-47-00744<span>|</span>
+          사업자등록번호: 651-47-00744<span className="separator">|</span>
           <strong>대표자: 남조아</strong>
         </p>
         <p>
-          남성훈@gmail.com<span>|</span>
+          남성훈@gmail.com<span className="separator">|</span>
           <span>010-1234-4567</span>
         </p>
         <small>Copyright &copy;2021 WISELY. All rights reserved.</small>
