c8a1fda6936b4b07cd45f464087708047737ee5d
alpaca <zxcvbnm5288@gmail.com>
2021-10-30 17:19:32 +0900

    add scroll to top

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./scss/App.scss";
 
+const ScrollToTop = () => {
+  window.scrollTo(0, 0);
+  return null;
+};
+
 export default function App() {
   return (
     <>
@@ -19,6 +24,7 @@ export default function App() {
           <Route path="/order" component={Order}></Route>
           <Route path="/about" component={About}></Route>
           <Route path="/cs" component={Cs}></Route>
+          <Route component={ScrollToTop}></Route>
         </main>
         <Footer />
       </HashRouter>
