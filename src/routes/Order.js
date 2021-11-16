e4d0b11347202c824c7af89dce03402c1659e476
alpaca <zxcvbnm5288@gmail.com>
2021-11-16 09:49:02 +0900

    update order page

--- src/routes/Order.js
+++ src/routes/Order.js
@@ -10,9 +10,14 @@ export default function Order() {
             <h3 className="order__title">
               Step 01: <span className="order__title--point">문자 주문</span>
             </h3>
-            <p>
-              <a href="tel:010-5024-6461">010-5024-6461</a>로 <br />
-              성함, 연락처, 희망상품, 희망수량, 주소를 적어서 보내주세요.
+            <p className="order__text">
+              <a className="order__link" href="tel:010-5024-6461">010-5024-6461</a>로
+              <br />
+              <strong className="order__text--point">
+                성함, 연락처, 희망 상품, 희망수량, 주소
+              </strong>
+              를 <br />
+              보내주세요.
             </p>
           </div>
           <FontAwesomeIcon icon={faChevronDown} />
@@ -23,8 +28,8 @@ export default function Order() {
               Step 01:{" "}
               <span className="order__title--point">카카오톡 주문</span>
             </h3>
-            <p>
-              <a
+            <p className="order__text">
+              <a className="order__link"
                 href="https://pf.kakao.com/_xdlHzb/chat"
                 target="_blank"
                 rel="noreferrer"
@@ -32,7 +37,12 @@ export default function Order() {
                 남송FS 카카오톡
               </a>
               으로 <br />
-              성함, 연락처, 희망상품, 희망수량, 주소를 적어서 보내주세요.
+              <strong className="order__text--point">
+                성함, 연락처, 희망 상품, 희망수량, 주소
+              </strong>
+              를
+              <br />
+              보내주세요.
             </p>
           </div>
           <FontAwesomeIcon icon={faChevronDown} />
@@ -43,8 +53,10 @@ export default function Order() {
               Step 02:{" "}
               <span className="order__title--point">주문 대금 입금</span>
             </h3>
-            <p>
-              <span>신한은행 110-335-624426</span>
+            <p className="order__text">
+              <strong className="order__text--point">
+                신한은행 110-335-624426
+              </strong>
               으로 돈내놔
             </p>
           </div>
@@ -55,9 +67,8 @@ export default function Order() {
             <h3 className="order__title">
               Step 03: <span className="order__title--point">입금확인</span>
             </h3>
-            <p>
-              <span>신한은행 110-335-624426</span>
-              으로 한 번 더 보내면 2배 찬스
+            <p className="order__text">
+              입금 확인은 24시간 이내에 이루어 집니다.
             </p>
           </div>
           <FontAwesomeIcon icon={faChevronDown} />
@@ -67,7 +78,7 @@ export default function Order() {
             <h3 className="order__title">
               Step 04: <span className="order__title--point">배송</span>
             </h3>
-            <p>
+            <p className="order__text">
               주문 접수 확인 후 다음 날 택배 발송하며,
               <br />
               발송 후 송장번호 안내드리고 있습니다.
