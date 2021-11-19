a89999dac292cc8080ed155e1bdfb5f020cdaaeb
alpaca <zxcvbnm5288@gmail.com>
2021-11-20 01:13:02 +0900

    add mobile header

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,33 @@
+import MediaQuery from "react-responsive";
+
 export default function Footer() {
   return (
     <footer className="footer">
-      <div className="footer__wrap inner">
-        <div>
-          <h5 className="footer__title">남송FS</h5>
-          <address>
-            경기도 수원시 권선구 서호서로33번길 27, 1층 102호(서둔동)
-          </address>
-          <p>
-            사업자등록번호: 651-47-00744<span className="separator">|</span>
-            <strong>대표자: 남조아</strong>
-          </p>
-          <p>
-            namsf17@naver.com<span className="separator">|</span>
-            <span>010-5024-6461</span>
-          </p>
-          <small>Copyright &copy;2021 NAMSONGFS. All rights reserved.</small>
+      <MediaQuery minWidth={768}>
+        <div className="footer__wrap inner">
+          <div>
+            <h5 className="footer__title">남송FS</h5>
+            <address>
+              경기도 수원시 권선구 서호서로33번길 27, 1층 102호(서둔동)
+            </address>
+            <p>
+              사업자등록번호: 651-47-00744<span className="separator">|</span>
+              <strong>대표자: 남조아</strong>
+            </p>
+            <p>
+              namsf17@naver.com<span className="separator">|</span>
+              <span>010-5024-6461</span>
+            </p>
+            <small>Copyright &copy;2021 NAMSONGFS. All rights reserved.</small>
+          </div>
+          <div className="footer__logos">
+            <p className="footer__logo logo__haccp">해썹</p>
+          </div>
         </div>
-        <div className="footer__logos">
-          <p className="footer__logo logo__haccp">해썹</p>
-        </div>
-      </div>
+      </MediaQuery>
+      <MediaQuery maxWidth={768}>
+        <div>1</div>
+      </MediaQuery>
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,49 +1,139 @@
+import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import MediaQuery from "react-responsive";
+import { Box, Drawer, Button, List, ListItem } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const [state, setState] = React.useState(false);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: 300 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <nav>
+          <ListItem>
+            <NavLink
+              activeStyle={{ fontWeight: "500", color: "rgb(0, 195, 137)" }}
+              to="/order"
+            >
+              주문 배송 절차
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              activeStyle={{ fontWeight: "500", color: "rgb(0, 195, 137)" }}
+              to="/products"
+            >
+              상품 보기
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              activeStyle={{ fontWeight: "500", color: "rgb(0, 195, 137)" }}
+              to="/about"
+            >
+              남송 이야기
+            </NavLink>
+          </ListItem>
+          <ListItem>
+            <NavLink
+              activeStyle={{ fontWeight: "500", color: "rgb(0, 195, 137)" }}
+              to="/cs"
+            >
+              고객센터
+            </NavLink>
+          </ListItem>
+        </nav>
+      </List>
+    </Box>
+  );
+
   return (
     <header className="header">
-      <div className="header__wrap inner">
-        <Link to="/" className="logo">
-          남송FS
-        </Link>
-        <nav>
-          <ul className="gnb">
-            <li>
-              <NavLink
-                activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
-                to="/order"
-              >
-                주문 배송 절차
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
-                to="/products"
-              >
-                상품 보기
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
-                to="/about"
-              >
-                남송 이야기
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
-                to="/cs"
-              >
-                고객센터
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <MediaQuery minWidth={768}>
+        <div className="header__wrap inner">
+          <Link to="/" className="logo">
+            남송FS
+          </Link>
+          <nav>
+            <ul className="gnb">
+              <li>
+                <NavLink
+                  activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
+                  to="/order"
+                >
+                  주문 배송 절차
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
+                  to="/products"
+                >
+                  상품 보기
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
+                  to="/about"
+                >
+                  남송 이야기
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  activeStyle={{ fontWeight: "600", color: "rgb(0, 195, 137)" }}
+                  to="/cs"
+                >
+                  고객센터
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </MediaQuery>
+      {/* mobile header */}
+      <MediaQuery maxWidth={768}>
+        <div className="header__wrap">
+          <Link to="/" className="logo">
+            남송FS
+          </Link>
+          <>
+            <Button onClick={toggleDrawer("right", true)}>
+              <FontAwesomeIcon
+                icon={faBars}
+                size="lg"
+                style={{ color: "rgb(0, 195, 137)" }}
+              />
+            </Button>
+            <Drawer
+              anchor="right"
+              open={state["right"]}
+              onClose={toggleDrawer("right", false)}
+            >
+              {list("right")}
+            </Drawer>
+          </>
+        </div>
+      </MediaQuery>
     </header>
   );
 }
