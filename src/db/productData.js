1bf0723abef5d2d483451e9c5f1a68cd0f7102dd
alpaca <zxcvbnm5288@gmail.com>
2021-11-26 10:37:48 +0900

    add product subscript

--- src/db/productData.js
+++ src/db/productData.js
@@ -3,17 +3,17 @@ import image02 from "../images/product_02.png";
 
 export const products = [
   {
-    title: "제목",
-    subscript: "설명",
-    price: "3,000,000원 (20g * 12개)",
+    title: "우동 건더기",
+    subscript: "건김, 건미역, 건파, 계란지단, 방울튀김, 건조맛살어묵",
+    price: "80,000원 (250g * 20개)",
     imageSrc: image01,
     imageAlt: "우동 건더기 사진",
   },
   {
-    title: "제목",
-    subscript: "설명",
-    price: "3,000,000원 (20g * 12개)",
+    title: "후리가깨",
+    subscript: "검정깨, 참깨, 건당근, 건대파, 난황후레이크, 코코넛칩, 정제염",
+    price: "60,000원 (140g * 20개)",
     imageSrc: image02,
-    imageAlt: "후리카케 사진",
+    imageAlt: "후리가깨 사진",
   },
 ];
